69397e390875c38df016ad455f15c606601148e1
Marcelo Garcia <marcelo_garcia@pense.com.br>
2015-05-19 17:20:25 -0300

    Commit

---
+++ remoteNotifiers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  remoteNotifiers
+//
+//  Created by Marcelo Garcia on 5/18/15.
+//  Copyright (c) 2015 Marcelo Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ remoteNotifiers/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  remoteNotifiers
+//
+//  Created by Marcelo Garcia on 5/18/15.
+//  Copyright (c) 2015 Marcelo Garcia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
